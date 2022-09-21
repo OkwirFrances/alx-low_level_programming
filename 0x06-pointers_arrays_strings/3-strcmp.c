c739aa178d01d6e082f7e888b77dec9836abab14
Francis <okwirfrancis2010@gmail.com>
2022-09-21 21:04:58 +0300

    add _strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,17 +7,17 @@
 *other number otherwise.
 */
 
-int _strcmp(char *s1, char *s2)
+int _strcmp(char *str1, char *str2)
 {
 	int i = 0;
 	int cmp = 0;
 
 	while (cmp == 0)
 	{
-		if((*(str1 + i) == '\0') && (*(str2 + i) == '\0'))
+		if ((*(str1 + i) == '\0') && (*(str2 + i) == '\0'))
 			break;
 		cmp = *(str1 + i) - *(str2 + i);
 		i++;
 	}
-	return cmp;
+	return (cmp);
 }
